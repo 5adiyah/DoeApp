51c26fbb2c611919e66841d7f3b6f22707827ab3
5adiyah <sadiali@pdx.edu>
2016-07-08 12:30:29 -0700

    Add model get methods

---
+++ app/src/main/java/com/example/guest/daughtersofeve/Photo.java
@@ -0,0 +1,22 @@
+package com.example.guest.daughtersofeve;
+
+/**
+ * Created by Guest on 7/8/16.
+ */
+public class Photo {
+    private String mCaption;
+    private String mUrl;
+
+    public Photo(String caption, String url){
+        this.mCaption = caption;
+        this.mUrl = url;
+    }
+
+    public String getCaption(){
+        return mCaption;
+    }
+
+    public String getUrl(){
+        return mUrl;
+    }
+}
--- app/src/main/java/com/example/guest/daughtersofeve/Photos.java
+++
@@ -1,7 +0,0 @@
-package com.example.guest.daughtersofeve;
-
-/**
- * Created by Guest on 7/8/16.
- */
-public class Photos {
-}
