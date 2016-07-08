723d8e27a274454c1b48de27547929c26ef1afa6
5adiyah <sadiali@pdx.edu>
2016-07-08 09:35:39 -0700

    Add credentials to gradle.properties

---
+++ app/src/main/java/com/example/guest/daughtersofeve/Constants.java
@@ -0,0 +1,10 @@
+package com.example.guest.daughtersofeve;
+
+/**
+ * Created by Guest on 7/8/16.
+ */
+public class Constants {
+    public static final String ClientId = BuildConfig.ClientId;
+    public static final String ClientSecret = BuildConfig.ClientSecret;
+
+}
