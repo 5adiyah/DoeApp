10762f62e056038f171f5810bd79e6f249e29eeb
5adiyah <sadiali@pdx.edu>
2016-07-08 12:09:08 -0700

    Calling a Request Asynchronously

--- app/src/main/java/com/example/guest/daughtersofeve/InstagramService.java
+++ app/src/main/java/com/example/guest/daughtersofeve/InstagramService.java
@@ -1,16 +1,16 @@
 package com.example.guest.daughtersofeve;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
-/**
- * Created by Guest on 7/8/16.
- */
+
 public class InstagramService {
-    public static void findImages(){
+    public static void findImages(Callback callback){
         OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.ClientId, Constants.ClientSecret);
 
         OkHttpClient client = new OkHttpClient.Builder()
@@ -24,6 +24,9 @@ public class InstagramService {
         Request request = new Request.Builder()
                 .url(url)
                 .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
 
