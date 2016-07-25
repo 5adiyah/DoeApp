02e7c33e56414eeaa60434a9a4309fdb1fd441ee
5adiyah <sadiali@pdx.edu>
2016-07-24 18:06:54 -0700

    Make clickable area larger in main activity

--- app/src/main/java/com/example/guest/daughtersofeve/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/daughtersofeve/ui/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.guest.daughtersofeve.ui.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,12 +23,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.popUpMenu) LinearLayout mPopUpMenu;
     @Bind(R.id.toggleMenu) ImageView mToggleMenu;
     @Bind(R.id.previousPage) ImageView mPreviousPage; //Change for each page
+
     @Bind(R.id.profileAbout) ImageView mProfileAbout;
+    @Bind(R.id.profileAboutLayout) RelativeLayout mProfileAboutLayout;
+    @Bind(R.id.profileAboutText) TextView mProfileAboutText;
+
     @Bind(R.id.profileEvents) ImageView mProfileEvents;
+    @Bind(R.id.profileEventsLayout) RelativeLayout mProfileEventsLayout;
+    @Bind(R.id.profileEventsText) TextView mProfileEventsText;
+
     @Bind(R.id.profilePhotos) ImageView mProfilePhotos;
+    @Bind(R.id.profilePhotosLayout) RelativeLayout mProfilePhotosLayout;
+    @Bind(R.id.profilePhotosText) TextView mProfilePhotosText;
+
     @Bind(R.id.profileTestimonial) ImageView mProfileTestimonial;
+    @Bind(R.id.profileTestimonialLayout) RelativeLayout mProfileTestimonialLayout;
+    @Bind(R.id.profileTestimonialText) TextView mProfileTestimonialText;
+
     @Bind(R.id.profileAccount) ImageView mProfileAccount;
+    @Bind(R.id.profileAccountLayout) RelativeLayout mProfileAccountLayout;
+    @Bind(R.id.profileAccountText) TextView mProfileAccountText;
+
     @Bind(R.id.logoutButton) ImageView mLogoutButton;
+
+
     private boolean viewGroupIsVisible = false;
 
     @Override
@@ -39,11 +58,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mToggleMenu.setOnClickListener(this);
         mPreviousPage.setOnClickListener(this); //Add page it goes to
+
         mProfileAbout.setOnClickListener(this);
+        mProfileAboutLayout.setOnClickListener(this);
+        mProfileAboutText.setOnClickListener(this);
+
         mProfileEvents.setOnClickListener(this);
+        mProfileEventsLayout.setOnClickListener(this);
+        mProfileEventsText.setOnClickListener(this);
+
         mProfilePhotos.setOnClickListener(this);
+        mProfilePhotosLayout.setOnClickListener(this);
+        mProfilePhotosText.setOnClickListener(this);
+
         mProfileTestimonial.setOnClickListener(this);
+        mProfileTestimonialLayout.setOnClickListener(this);
+        mProfileTestimonialText.setOnClickListener(this);
+
         mProfileAccount.setOnClickListener(this);
+        mProfileAccountLayout.setOnClickListener(this);
+        mProfileAccountText.setOnClickListener(this);
+
         mLogoutButton.setOnClickListener(this);
     }
 
@@ -58,19 +93,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             viewGroupIsVisible = !viewGroupIsVisible;
         } else if(v == mLogoutButton){
             logout();
-        } else if(v == mProfileAbout){
+        }
+
+        else if(v == mProfileAbout){
             Intent intent = new Intent(MainActivity.this, AboutActivity.class);
             startActivity(intent);
-        }else if(v == mProfileEvents){
+        }else if(v == mProfileAboutLayout){
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }else if(v == mProfileAboutText){
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        }
+
+        else if(v == mProfileEvents){
             Intent intent = new Intent(MainActivity.this, EventsActivity.class);
             startActivity(intent);
-        }else if(v == mProfilePhotos){
+        }else if(v == mProfileEventsLayout){
+            Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+            startActivity(intent);
+        }else if(v == mProfileEventsText){
+            Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+            startActivity(intent);
+        }
+
+        else if(v == mProfilePhotos){
             Intent intent = new Intent(MainActivity.this, PhotosActivity.class);
             startActivity(intent);
-        }else if(v == mProfileTestimonial){
+        }else if(v == mProfilePhotosLayout){
+            Intent intent = new Intent(MainActivity.this, PhotosActivity.class);
+            startActivity(intent);
+        }else if(v == mProfilePhotosText){
+            Intent intent = new Intent(MainActivity.this, PhotosActivity.class);
+            startActivity(intent);
+        }
+
+        else if(v == mProfileTestimonial){
             Intent intent = new Intent(MainActivity.this, TestimonialActivity.class);
             startActivity(intent);
-        }else if(v == mProfileAccount){
+        }else if(v == mProfileTestimonialLayout){
+            Intent intent = new Intent(MainActivity.this, TestimonialActivity.class);
+            startActivity(intent);
+        }else if(v == mProfileTestimonialText){
+            Intent intent = new Intent(MainActivity.this, TestimonialActivity.class);
+            startActivity(intent);
+        }
+
+        else if(v == mProfileAccount){
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else if(v == mProfileAccountLayout){
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        }else if(v == mProfileAccountText){
             Intent intent = new Intent(MainActivity.this, MainActivity.class);
             startActivity(intent);
         }
